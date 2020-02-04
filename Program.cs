795105f166609ee7fa5e5dc44c2c41d9607c632b
Pietro <pietro.carillo@gmail.com>
2020-02-04 16:01:50 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace pcarillo_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, what's your name?"); // display message asking for name
+            string name = Console.ReadLine(); // collect user input and store it inside the variable name
+            Console.WriteLine("Hello " + name + "!"); // print strings along with variable name
+        }
+    }
+}
