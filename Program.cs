f8591849a1380a92fce340d4d62be978138f80d9
Pietro Caldarone Carillo <pietro.carillo@gmail.com>
2020-02-06 17:29:31 -0500

    Commit all

--- Program.cs
+++ Program.cs
@@ -7,31 +7,31 @@ namespace pcarillo_methods
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter your name:"); // get user input
-            string name = HelloName(); // call method and save it in a variable
-            if (System.Text.RegularExpressions.Regex.IsMatch(name, "[a-zA-Z]" + " " + "[a-zA-Z]")) // test if user entered string or not
+            HelloName(); // call method and save it in a variable
+        
+            // create method
+            void HelloName()
             {
-                // display message to user
-                Console.WriteLine("Hello" + " " + name + "!");
-                Console.WriteLine("Thank you for giving an acceptable input.");
+                string name = Console.ReadLine(); // collect user input
+                if (System.Text.RegularExpressions.Regex.IsMatch(name, "[a-zA-Z]" + " " + "[a-zA-Z]+$")) // test if user entered string or not
+                {
+                    // display message to user
+                    Console.WriteLine("Hello" + " " + name + "!");
+                    Console.WriteLine("Thank you for giving an acceptable input.");
+                }
+                else if (System.Text.RegularExpressions.Regex.IsMatch(name, @"^[a-zA-Z]+$"))
+                {
+                    // display message to user
+                    Console.WriteLine("Hello" + " " + name + "!");
+                    Console.WriteLine("Thank you for giving an acceptable input.");
+                }
+                else
+                {
+                    // display error message to user
+                    Console.WriteLine("Please only enter alphabetical letters.");
+                    Console.WriteLine("Please enter your name and try again.");
+                }
             }
-            else if (System.Text.RegularExpressions.Regex.IsMatch(name, "[a-zA-Z]"))
-            {
-                // display message to user
-                Console.WriteLine("Hello" + " " + name + "!");
-                Console.WriteLine("Thank you for giving an acceptable input.");
-            }
-            else
-            {
-                // display error message to user
-                Console.WriteLine("Please only enter alphabetical letters.");
-                Console.WriteLine("Please enter your name and try again.");
-            }
-        }
-        // create method
-        private static string HelloName()
-        {
-            string name = Console.ReadLine(); // collect user input
-            return name; // return input
         }
     }
 }
