d85b2d09ac86ec43d09b53f069e24f20571c3d45
Pietro Caldarone Carillo <pietro.carillo@gmail.com>
2020-02-06 12:09:10 -0500

    Commit all

--- Program.cs
+++ Program.cs
@@ -8,7 +8,24 @@ namespace pcarillo_methods
         {
             Console.WriteLine("Please enter your name:"); // get user input
             string name = HelloName(); // call method and save it in a variable
-            Console.WriteLine("Hello " + name + "!"); // show message to user input
+            if (System.Text.RegularExpressions.Regex.IsMatch(name, "[a-zA-Z]" + " " + "[a-zA-Z]")) // test if user entered string or not
+            {
+                // display message to user
+                Console.WriteLine("Hello" + " " + name + "!");
+                Console.WriteLine("Thank you for giving an acceptable input.");
+            }
+            else if (System.Text.RegularExpressions.Regex.IsMatch(name, "[a-zA-Z]"))
+            {
+                // display message to user
+                Console.WriteLine("Hello" + " " + name + "!");
+                Console.WriteLine("Thank you for giving an acceptable input.");
+            }
+            else
+            {
+                // display error message to user
+                Console.WriteLine("Please only enter alphabetical letters.");
+                Console.WriteLine("Please enter your name and try again.");
+            }
         }
         // create method
         private static string HelloName()
